969429df8d8a6e7f7fc5def84da8071a093e55f6
EuphoricLeagueSharp <EuphoricLeagueSharp@gmail.com>
2015-08-31 18:29:27 -0500

    *

---
+++ Euphoric Gentleman/Program.cs
@@ -0,0 +1,26 @@
+﻿using LeagueSharp.Common;
+using LeagueSharp;
+using System;
+
+namespace Euphoric_Gentleman
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnLoad;
+            CustomEvents.Game.OnGameEnd += Game_OnEnd;
+        }
+
+        private static void Game_OnLoad(EventArgs args)
+        {
+            Game.PrintChat("<font color=\"#55FF00\"><b>Euphoric</font> <font color=\"#00AAFF\">Gentleman</b></font> successfully loaded!");
+            Utility.DelayAction.Add((new Random(Environment.TickCount).Next(1000, 10001)), () => Game.Say("/all glhf"));
+        }
+
+        private static void Game_OnEnd(EventArgs args)
+        {
+            Utility.DelayAction.Add((new Random(Environment.TickCount).Next(100, 1001)), () => Game.Say("/all ggwp"));
+        }
+    }
+}
