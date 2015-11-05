44a17b756ea30cccb7fc680d5f96b30db6e9041c
EuphoricLeagueSharp <EuphoricLeagueSharp@gmail.com>
2015-11-04 21:32:36 -0600

    *

--- Euphoric Gentleman/Program.cs
+++ Euphoric Gentleman/Program.cs
@@ -16,13 +16,13 @@ namespace Euphoric_Gentleman
         {
             Game.PrintChat("<font color=\"#00AAFF\"><b>Euphoric Gentleman</b></font> successfully loaded!");
             Utility.DelayAction.Add(new Random(Environment.TickCount).Next(25000, 50001),
-                () => Game.Say("Good luck, have fun!"));
+                () => Game.Say("/all Good luck, have fun!"));
         }
 
         private static void Game_OnEnd(EventArgs args)
         {
             Utility.DelayAction.Add(new Random(Environment.TickCount).Next(250, 501),
-                () => Game.Say("Good game, well played!"));
+                () => Game.Say("/all Good game, well played!"));
         }
 
     }
